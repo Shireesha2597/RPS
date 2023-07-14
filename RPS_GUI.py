2be9c7765fee2d90fd651ae6de30c7407d96c685
Shireesha2597 <139561080+Shireesha2597@users.noreply.github.com>
2023-07-14 14:20:18 -0400

    Update RPS_GUI.py

--- RPS_GUI.py
+++ RPS_GUI.py
@@ -1,134 +1,64 @@
-# We will use the Tkinter and random module of Python to implement this python rock paper scissors game.
-
 #Tkinter is a standard GUI library that creates GUI apps easily.
 #The random module is used to generate random numbers.
 #RPS by Shireesha
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 import random
-import tkinter
-user = int
-computer = int
-win = 0
-lose = 0
-def rps(win, lose, user):
-    computer = random.randrange(1,4)
-    if user == computer:
-        var.set("It's a draw. \n No Points")  
-    elif user == 1 and computer == 3:
-        var.set("You chose Rock, I chose Scissors. \nYou win")
-        wins.set(wins.get() + 1)
-            
-    elif user == 1 and computer == 2:
-        var.set("You chose Rock, I chose Paper. \nYou lose")
-        lose += 1
-        wins.set(wins.get() - 1)    
-    elif user == 2 and computer == 1:
-        var.set("You chose Paper, I chose Rock. \nYou win")
-        wins.set(wins.get() + 1)
-        wins.set(wins.get() - 1)    
-    elif user == 2 and computer == 3:
-        var.set("You chose Paper, I chose Scissors. \nYou lose")
-        lose += 1
-        wins.set(wins.get() - 1)   
-    elif user == 3 and computer == 1:
-        var.set("You chose Scissors, I chose Rock. \nYou lose")
-        lose += 1
-        wins.set(wins.get() - 1)    
-    elif user == 3 and computer == 2:
-        var.set("You chose Scissors, I chose Paper. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 4 and computer == 3:
-        var.set("You chose Spock, I chose Scissors. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 4 and computer == 1:
-        var.set("You chose Spock, I chose Rock. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 4 and computer == 5:
-        var.set("You chose Spock, I chose Lizard. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 4 and computer == 2:
-        var.set("You chose Spock, I chose Paper. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 5 and computer == 1:
-        var.set("You chose Lizard, I chose Rock. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 5 and computer == 2:
-        var.set("You chose Lizard, I chose Paper. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 5 and computer == 3:
-        var.set("You chose Lizard, I chose Scissors. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 5 and computer == 4:
-        var.set("You chose Lizard, I chose Spock. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 1 and computer == 4:
-        var.set("You chose Rock, I chose Spock. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 2 and computer == 4:
-        var.set("You chose Paper, I chose Spock. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 3 and computer == 4:
-        var.set("You chose Scissors, I chose Spock. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 5 and computer == 4:
-        var.set("You chose Lizard, I chose Spock. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 1 and computer == 5:
-        var.set("You chose Rock, I chose Lizard. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 2 and computer == 5:
-        var.set("You chose Paper, I chose Lizard. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)
-    elif user == 3 and computer == 5:
-        var.set("You chose Scissors, I chose Lizard. \nYou win")
-        wins.set(wins.get() + 1)
-        
-    elif user == 4 and computer == 5:
-        var.set("You chose Spock, I chose Lizard. \nYou lose")
-        lose +=1
-        wins.set(wins.get() - 1)  
+
+# Create the main window
+window = tk.Tk()
+window.title("Rock Paper Scissors Game")
+choices = ["Rock", "Paper", "Scissors"]
+
+def play_game(player_choice):
+    computer_choice = random.choice(choices)
+
+    if player_choice == computer_choice:
+        result = "It's a tie!"
+    elif (
+        (player_choice == "Rock" and computer_choice == "Scissors") or
+        (player_choice == "Paper" and computer_choice == "Rock") or
+        (player_choice == "Scissors" and computer_choice == "Paper")
+    ):
+        result = "You win!"
     else:
-        var.set("Thanks for playing. \nYou have " + str(win) + " wins and " + str(lose) + " losses.")
+        result = "You lose!"
 
+    messagebox.showinfo("Result", f"Your choice: {player_choice}\nComputer's choice: {computer_choice}\n\n{result}")
 
-    
-top = tkinter.Tk()
-top.wm_title("RPS Python GUI")
-top.minsize(width=350, height=150)
-top.maxsize(width=350, height=150)
-B1 = tkinter.Button(top, text ="Rock", command = lambda: rps(win, lose, 1))
-B1.grid(row=0, column=1)
-B2 = tkinter.Button(top, text ="Paper", command = lambda: rps(win, lose, 2))
-B2.grid(row=0, column=2)
-B3 = tkinter.Button(top, text ="Scissors", command = lambda: rps(win, lose, 3))
-B3.grid(row=0, column=3)
-space = tkinter.Label(top, text="")
-space.grid(row=1)
-var = StringVar()
-var.set('Welcome!')
-l = Label(top, textvariable = var)
-l.grid(row=2, column=2)
-wins = IntVar()
-wins.set(win)
-w = Label(top, textvariable = wins)
-w.grid(row=4, column=2)
-labeled = Label(top, text = "Score:")
-labeled.grid(row=3, column=2)
-copy = Label(top, text= "RPS GUI by Siri")
-copy.grid(row=5, column=2)
-top.mainloop()
+def play_again():
+    play_game_window = tk.Toplevel(window)
+    play_game_window.title("Play Rock Paper Scissors")
+
+    # Create radio buttons for player choice
+    player_choice_var = tk.StringVar(play_game_window)
+    player_choice_var.set(choices[0])
+
+    for choice in choices:
+        tk.Radiobutton(play_game_window, text=choice, variable=player_choice_var, value=choice).pack()
+
+    # Create a button to play the game
+    play_button = tk.Button(
+        play_game_window,
+        text="Play",
+        command=lambda: play_game(player_choice_var.get())
+    )
+    play_button.pack()
+
+    # Focus the play button for better usability
+    play_button.focus()
+
+def start_game():
+    # Create a button to play the game
+    play_button = tk.Button(
+        window,
+        text="Play Rock Paper Scissors",
+        command=play_again
+    )
+    play_button.pack()
+# Call the start_game function to display the "Play Rock Paper Scissors" button
+start_game()
+
+# Start the GUI event loop
+window.mainloop()
+
